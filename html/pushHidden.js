42826846a79142164a27a4cd6a657bf92c351cdf
Risky Jr <2114768901@qq.com>
2021-09-08 18:36:58 +0800

    添加按钮

--- html/pushHidden.js
+++ html/pushHidden.js
@@ -1,15 +1,24 @@
 // 点击实现隐藏\显示功能
 window.onload = function () {
-  var obt = document.getElementById("btn-1");
+  var obt = document.getElementById("btn-pie");
   var odiv = document.getElementById("bar_1");
-  var bodybg = document.getElementsByTagName("body")
   obt.onclick = function () {
     if (odiv.style.display == "none") {
       odiv.style.display = "block";
-      obt.value = "隐藏模块";
     } else {
       odiv.style.display = "none";
-      obt.value = "显示模块";
+    }
+  };
+};
+
+window.onload = function () {
+  var obt = document.getElementById("btn-bar");
+  var odiv = document.getElementById("bar_1");
+  obt.onclick = function () {
+    if (odiv.style.display == "none") {
+      odiv.style.display = "block";
+    } else {
+      odiv.style.display = "none";
     }
   };
 };
