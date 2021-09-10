4478c3b62cd5afe5bb37976b2d850bf57a6975bc
Risky Jr <2114768901@qq.com>
2021-09-10 17:32:14 +0800

    iframe 网页调整

--- html/pushHidden.js
+++ html/pushHidden.js
@@ -10,7 +10,22 @@ window.onload = function () {
     }
   };
 };
-
+window.onload = function () {
+  var obt = document.getElementById("btn-pie");
+  //获取iframe的window对象
+  var topWin = window.top.document.getElementById("pie_iframe").contentWindow;
+  //通过获取到的window对象操作HTML元素，这和普通页面一样
+  var oiframe = (topWin.document.getElementById(
+    "5a156e67104a454881553d9f547be356"
+  ).style.visibility = "visible");
+  obt.onclick = function () {
+    if (odiv.style.display == "none") {
+      odiv.style.display = "block";
+    } else {
+      odiv.style.display = "none";
+    }
+  };
+};
 window.onload = function () {
   var obt = document.getElementById("btn-bar");
   var odiv = document.getElementById("bar_1");
@@ -22,3 +37,15 @@ window.onload = function () {
     }
   };
 };
+
+function adjustIframe() {
+  var ifm = document.getElementById("pie_iframe");
+  ifm.height = document.documentElement.clientHeight;
+  ifm.width = document.documentElement.clientWidth;
+  var ifm = document.getElementById("bar_iframe");
+  ifm.height = document.documentElement.clientHeight;
+  ifm.width = document.documentElement.clientWidth;
+  var ifm = document.getElementById("geo_iframe");
+  ifm.height = document.documentElement.clientHeight;
+  ifm.width = document.documentElement.clientWidth;
+}
