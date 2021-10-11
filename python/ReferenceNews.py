ded5173944ecd9c907043f66598d9b2d28255bd1
Risky Jr <2114768901@qq.com>
2021-10-11 15:25:44 +0800

    爬虫-参考消息，下载本地

--- python/ReferenceNews.py
+++ python/ReferenceNews.py
@@ -15,9 +15,17 @@ import os
 class RefNews():
     BaseUrl = r"http://www.jdqu.com"
     html = 0
+    picDir = ''
+    filePath = ''
+    htmlFile = ''
+    pageDate = ''
 
     def __init__(self, *pk, **pkw):
         super().__init__()
+        # change workspace into script dir
+        self.filePath = os.path.split(os.path.abspath(__file__))[0]
+        os.chdir(self.filePath)
+        # get base web
         self.headers = {
             'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31'
@@ -30,7 +38,8 @@ class RefNews():
         todayRnsLink = self._GetPaperLink()
         validSubUrls = self._ParseValidPageUrl(
             todayRnsLink[0], todayRnsLink[1])
-        self._DownLoadPagePicture(validSubUrls)
+        if not validSubUrls is None:
+            self._DownLoadPagePicture(validSubUrls)
 
     def _GetPaperLink(self, *pk, **pkw):
         newspaper = dict()
@@ -54,10 +63,13 @@ class RefNews():
     def _ParseValidPageUrl(self, papername="", paperbaselink="", *pk, **pkw):
         name = papername
         mat = re.findall(r"(\d{4}-\d{1,2}-\d{1,2})", name)
-        if datetime.strptime(mat[0], '%Y-%m-%d').date() == date.today():
+        self.pageDate = mat[0]
+        # folder to save picture
+        if datetime.strptime(self.pageDate, '%Y-%m-%d').date() == date.today():
             print("Today News", papername)
         else:
             print("Yesterday Flower", papername)
+
         # check for valid picture page url
         subUrl = paperbaselink.replace('.html', '')
         validPageLinks = list()
@@ -76,21 +88,37 @@ class RefNews():
             return
         return validPageLinks
 
-    def _DownLoadPagePicture(self, urls=[], *k, **kw):
-        if not os.path.isdir(r'./NewsImg'):
-            os.mkdir(r'./NewsImg')
-        NewsImgAbsPath = os.path.abspath('./NewsImg')
-        print(NewsImgAbsPath)
+    def _DownLoadPagePicture(self, urls=[], *pk, **pkw):
+        # picture folder
+        self.picDir = os.path.join(self.filePath, 'NewsPic')
+        if not os.path.isdir(self.picDir):
+            os.mkdir(self.picDir)
+        self.picDir = os.path.join(self.picDir, self.pageDate)
+        # specific day's folder
+        if not os.path.isdir(self.picDir):
+            os.mkdir(self.picDir)
         for url in urls:
             html = requests.get(url, headers=self.headers)
             html.encoding = html.apparent_encoding
             htmlBs = BeautifulSoup(html.text, "lxml")
             imgHtml = htmlBs.select('img')
-            imgName = imgHtml[0].get('alt')
+            # imgName = imgHtml[0].get('alt')
             imgUrl = imgHtml[0].get('src')
-            print(imgName, imgUrl)
-            # with open(imgUrl[0].get('src'), 'wb') as f:
-            #     f.write()
+            curPic = os.path.join(self.picDir, imgUrl.split('/')[-1])
+            # print(curPic)
+
+            r = requests.get(imgUrl)
+            with open(curPic, "wb")as f:
+                f.write(r.content)
+                f.close()
+            i = urls.index(url)
+            print('\rLoading：{0}{1}%'.format(
+                '▉'*(i+1), ((i+1)*100/len(urls))), end='')
+
+    def _WriteHtml(self, *pw, **pkw):
+        self.htmlFile = os.path.join(
+            self.picDir, 'ReferenceNews'+self.pageDate+'.html')
+        print(self.htmlFile)
 
 
 if __name__ == "__main__":
