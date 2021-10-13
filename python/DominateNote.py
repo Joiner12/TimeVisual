ca113d1e51c6786a2340d3050622bd66288fd9b0
Risky Jr <2114768901@qq.com>
2021-10-13 18:23:25 +0800

    dominate note -1

---
+++ python/DominateNote.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 18:04:22 2021
+
+@author: W-H
+"""
+import dominate
+from dominate.tags import *
+
+baseHtml = html(body(h1('Hello, World!')))
+print(baseHtml)
