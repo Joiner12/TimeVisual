0fa26065db4256667618ca213a8d86e58ebe1fc5
Risky_11 <2114768901@qq.com>
2021-11-05 18:39:23 +0800

    2021-11-05 18:39

--- python/AutoScritp.py
+++ python/AutoScritp.py
@@ -14,52 +14,61 @@ Created on Wed Nov  3 17:23:50 2021
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 from numpy import random
+import time
 def testWebdriver():
+    print('check for to do list')
+    """
+    embeded function
+    """
+    def wait(locator, timeout=2):
+        WebDriverWait(browser, timeout).until(
+            expected_conditions.presence_of_all_elements_located(locator))
+
     TargetBaseUrl = r'http://172.18.0.28:8080/cas/login?service=http://172.18.0.29/cas#/portal/index'
-    # TargetBaseUrl = r"file:///C:/Users/W-H/Desktop/login.htm"
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.40',
-    #     'Cookie': 'portal_2_uid=c9791325-f44e-4a05-a134-1f8e19eab0ce'
-    #     # portal_2_uid=c9791325-f44e-4a05-a134-1f8e19eab0ce
-    # }
-
-    # 以下ip使用自己可使用的代理IP
-    proxy_arr = [
-        '--proxy-server=http://171.35.141.103:9999',
-        '--proxy-server=http://36.248.132.196:9999',
-        # '--proxy-server=http://125.46.0.62:53281',
-        '--proxy-server=http://219.239.142.253:3128',
-        '--proxy-server=http://119.57.156.90:53281',
-        '--proxy-server=http://60.205.132.71:80',
-        '--proxy-server=https://139.217.110.76:3128',
-        '--proxy-server=https://116.196.85.150:3128'
-    ]
-
     chromeOptions = Options()
-
+    chromeOptions.add_argument('headless')
     browser = webdriver.Chrome(
         executable_path=r"D:\Code\TimeVisual\python\chromedriver.exe", options=chromeOptions)
 
     # browser.set_window_size(200, 200)
-
     browser.get(TargetBaseUrl)
-    # check login status
-    logInStatus = True
+    # login status
     longInButtonId = "normalLoginButton"
-    try:
-        browser.find_element_by_id(longInButtonId)
-    except:
-        print('no such element')
-        logInStatus = False
-
-    if logInStatus:
-        browser.find_element_by_id("username").clear()
-        browser.find_element_by_id("username").send_keys('wuhao3')
-        browser.find_element_by_id("password").clear()
-        browser.find_element_by_id("password").send_keys('chinamobile1_1')
+    wait((By.ID, longInButtonId))
+    browser.find_element_by_id(longInButtonId)
+    browser.find_element_by_id("username").clear()
+    browser.find_element_by_id("username").send_keys('wuhao3')
+    browser.find_element_by_id("password").clear()
+    browser.find_element_by_id("password").send_keys('chinamobile1_1')
     browser.find_element_by_id(longInButtonId).click()
-    # browser.quit()
+
+    try:
+        while True:
+            # scroll for load element js
+            browser.execute_script("window.scrollBy(0,3000)")
+            wait((By.XPATH,
+                  r'//*[@id="app"]/div/div/div[2]/div[3]/div/div[2]/div[2]/div[2]/div/ul/li[1]/a'))
+            elementUpcoming = browser.find_element_by_xpath(
+                r'//*[@id="app"]/div/div/div[2]/div[3]/div/div[2]/div[2]/div[2]/div/ul/li[1]/a')
+            print(elementUpcoming.text)
+            browser.execute_script("arguments[0].click();", elementUpcoming)
+            time.sleep(3+random.rand())
+            all_h = browser.window_handles
+            browser.switch_to.window(all_h[1])
+            browser.execute_script("window.scrollBy(0,3000)")
+            time.sleep(3+random.rand())
+            wait((By.CSS_SELECTOR, '.submit_btn.form_btn1'))
+            browser.execute_script(
+                "arguments[0].click();", browser.find_element_by_css_selector('.submit_btn.form_btn1'))
+            time.sleep(1+random.rand())
+    except:
+        print('empty to do list')
+
+    browser.quit()
 
 
 if __name__ == "__main__":
