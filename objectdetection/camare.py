0ee4fbd3b07fbddce06a386b18d79b0f4af13f41
Risky_11 <2114768901@qq.com>
2022-01-03 11:29:58 +0800

    camare测试完成

--- objectdetection/camare.py
+++ objectdetection/camare.py
@@ -10,6 +10,34 @@ import cv2
 import time
 
 
+def test_camare(camare_num, *args, **kwargs):
+    """测试摄像头
+
+    参数
+    ----------
+    camare_num : int
+        摄像头ID,笔记本电脑自带电脑为:0,外接设备为:1.
+
+    返回值
+    -------
+    None
+    """
+    cap = cv2.VideoCapture(camare_num)
+    print(cap.get(cv2.CAP_PROP_FPS))
+    # cap.get()
+    print("按键Q-结束测试")
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+        else:
+            print('摄像头打开失败')
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def extrac_frame(video_file, *args, **kwargs):
     """从视频中读取帧
 
@@ -38,8 +66,8 @@ def extrac_frame(video_file, *args, **kwargs):
     cv2.destroyAllWindows()
 
 
-def capture_camare_video():
-    """获取摄像头数据流
+def capture_motion_video():
+    """获取变化摄像头数据流
 
     参数
     ----------
@@ -49,7 +77,7 @@ def capture_camare_video():
     -------
     None
     """
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     codec = cv2.VideoWriter_fourcc(*'MJPG')
     fps = 20.0
     frameSize = (640, 480)
@@ -58,10 +86,10 @@ def capture_camare_video():
     while (cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
-            out.write(frame)
+            # out.write(frame)
             cv2.imshow('frame', frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
+            if cv2.waitKey(1) == ord('q'):
+                break
         else:
             break
     cap.release()
@@ -69,5 +97,42 @@ def capture_camare_video():
     cv2.destroyAllWindows()
 
 
+def average_hash(img, *args, **kwargs):
+    """均值哈希算法,用以比较两图相似度
+
+    参数
+    ----------
+    img : str
+        图像文件
+
+    返回值
+    -------
+    None
+    """
+    #缩放为8*8
+    img = cv2.resize(img, (8, 8))
+    #转换为灰度图
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #s为像素和初值为0，hash_str为hash值初值为''
+    s = 0
+    hash_str = ''
+    #遍历累加求像素和
+    for i in range(8):
+        for j in range(8):
+            s = s + gray[i, j]
+    #求平均灰度
+    avg = s / 64
+    #灰度大于平均值为1相反为0生成图片的hash值
+    for i in range(8):
+        for j in range(8):
+            if gray[i, j] > avg:
+                hash_str = hash_str + '1'
+            else:
+                hash_str = hash_str + '0'
+    return hash_str
+
+
 if __name__ == "__main__":
-    extrac_frame(r'WeChat_20211230162610.mp4')
+    # extrac_frame(r'WeChat_20211230162610.mp4')
+    # capture_motion_video()
+    test_camare(1)
