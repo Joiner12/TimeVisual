c83c5e88adade5d2a4d75f414e051ab09d4bca8a
Risky_11 <2114768901@qq.com>
2022-01-19 10:24:17 +0800

    完善虫部落自动签到

--- ToolPy/auto_sign_in.py
+++ ToolPy/auto_sign_in.py
@@ -9,16 +9,20 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
-import time
+from datetime import datetime
+from random import randint
+import re
 
 
 def log_in_main_page():
-    print(r"   ____ _                       _           _")
-    print(r"  / ___| |__   ___  _ __   __ _| |__  _   _| |_   _  ___")
-    print(r" | |   | '_ \ / _ \| '_ \ / _` | '_ \| | | | | | | |/ _ \ ")
-    print(r" | |___| | | | (_) | | | | (_| | |_) | |_| | | |_| | (_) |")
-    print(r"  \____|_| |_|\___/|_| |_|\__, |_.__/ \__,_|_|\__,_|\___/")
-    print(r"                          |___/")
+    print(r" __________________________________________________________")
+    print(r"|   ____ _                       _           _             |")
+    print(r"|  / ___| |__   ___  _ __   __ _| |__  _   _| |_   _  ___  |")
+    print(r"| | |   | '_ \ / _ \| '_ \ / _` | '_ \| | | | | | | |/ _ \ |")
+    print(r"| | |___| | | | (_) | | | | (_| | |_) | |_| | | |_| | (_) ||")
+    print(r"|  \____|_| |_|\___/|_| |_|\__, |_.__/ \__,_|_|\__,_|\___/ |")
+    print(r"|                          |___/                           |")
+    print(r"|__________________________________________________________|")
     """
     embeded function
     """
@@ -35,7 +39,7 @@ def log_in_main_page():
         "platform": "WINDOWS",
         "ms:edgeOptions": {
             'extensions': [],
-            'args': ['--headless']
+            # 'args': ['--headless']
         }
     }
     browser = webdriver.Edge(
@@ -59,12 +63,15 @@ def log_in_main_page():
         r'/html/body/div[6]/div[1]/div/div/ul/li[15]/a').click()
     #
     wait((By.XPATH, r'/html/body/div[5]/div[1]/div/a[2]'), timeout=10)
+    # 签到信息
+    continue_days = 'NaN'
+    got_bits = 'NaN'
+
     # 签到
     # /html/body/div[5]/div[2]/div[1]/div[1]/a
     sign_in_text = browser.find_element_by_xpath(
         r'/html/body/div[5]/div[2]/div[1]/div[1]/a').text
     if not '已签到' in sign_in_text:
-        print(True)
         browser.find_element_by_xpath(
             r'/html/body/div[5]/div[2]/div[1]/div[1]/a').click()
         wait((
@@ -72,15 +79,42 @@ def log_in_main_page():
             r'/html/body/div[1]/div/table/tbody/tr[2]/td[2]/form/div/p/textarea'
         ),
              timeout=10)
+        if False:
+            filling_words = datetime.now().strftime('%Y-%m-%d')
+        else:
+            filling_words = piece_lrc()
         browser.find_element_by_xpath(
             r'/html/body/div[1]/div/table/tbody/tr[2]/td[2]/form/div/p/textarea'
-        ).send_keys(str(time.asctime(time.localtime(time.time()))))
+        ).send_keys(filling_words)
         browser.find_element_by_xpath(
             r'/html/body/div[1]/div/table/tbody/tr[2]/td[2]/form/div/button'
         ).click()
+        # 签到完成后需要刷新页面
+        #
+        wait((By.XPATH, r'/html/body/div[5]/div[1]/div/a[2]'), timeout=10)
+        browser.find_element_by_xpath(
+            r'/html/body/div[5]/div[1]/div/a[2]').click()
     else:
-        print('已签到')
+        print('今日已签到,无需重复签到!')
+    #
+    continue_days = browser.find_element_by_xpath(
+        r'/html/body/div[5]/div[2]/div[1]/div[2]/ul/li[1]/span[1]').text
+    got_bits = browser.find_element_by_xpath(
+        r'/html/body/div[5]/div[2]/div[1]/div[2]/ul/li[2]/span').text
+    got_bits = re.sub(r'Bit', '', got_bits)
+    text_temp = "\t连续签到:%5s天 \t 累计获得:%4s Bit" % (continue_days, got_bits)
+    print('—' * 59 + '\n', text_temp.center(40, chr(12288)), '—' * 59)
     browser.quit()
+
+
+def piece_lrc():
+    ret_lrc = ""
+    with open(r'./lrc/eason.txt', encoding='utf-8', mode='r') as f:
+        all_lines = f.readlines()
+        line_cnt = len(all_lines)
+        ret_lrc = all_lines[randint(0, line_cnt - 1)]
+    ret_lrc = re.sub(r'\[\d{1,}\]:', '', ret_lrc)
+    return ret_lrc
 
 
 if __name__ == "__main__":
