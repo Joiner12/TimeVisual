0b202bb8c8e377d9af0cfec0d771530039f0b15c
Risky_11 <2114768901@qq.com>
2023-02-14 11:01:12 +0800

    修改xpath

--- ToolPy/AutoScript.py
+++ ToolPy/AutoScript.py
@@ -52,7 +52,7 @@ def testWebdriver():
         "ms:edgeOptions": {
             'extensions': [],
             'args': [
-                # '--headless'
+                '--headless'
                 # '--disable-gpu',
                 # '--remote-debugging-port=9222',
             ]
